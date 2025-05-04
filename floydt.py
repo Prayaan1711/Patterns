5df659c791d4758348c289b283af094a9a7d51df
Prayaan1711 <spyfyi1711@gmail.com>
2025-05-04 09:49:21 +0530

    1234

---
+++ floydt.py
@@ -0,0 +1,12 @@
+print("floyds triangle : ")
+n = int(input("Enter the number of rows: "))
+
+x = 1
+
+for i in range(n):
+
+    for j in range(i+1):
+
+        print(x, end = " ")
+        x = x+1
+    print()
